9b4aedafdf8bee803fdf61326315656642258c12
BaccoucheTahaGit <baccoutaha2@gmail.com>
2024-02-12 10:13:52 +0100

    add Search module "docusaurus-lunr-search"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,18 +13,21 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://mobee-app.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/developer-handbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Mobee-App', // Usually your GitHub org/user name.
+  projectName: 'developer-handbook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  plugins: [require.resolve('docusaurus-lunr-search')],
+
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
